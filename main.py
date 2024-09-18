e6f8d216643a853abba3682889877ce276c937fc
Amish Patel <64919993+Toast7529@users.noreply.github.com>
2024-09-18 18:49:27 +0100

    Added timed loop
    Will refresh after 12 hours
    Display will update at specified seconds (in the config)

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from config import loadConfig
-
+from display import Display
+from epd import EPD
+import time
+config = loadConfig('config.toml')
+epd = EPD()
 def main():
-    config = loadConfig('config.toml')
-    print("printing config:")
-    print(config)
+    display = Display("base.png")
+    startTime = time.time()
+    while True:
+        if time.time() - startTime >= 43200:    # refresh e ink every 12 hours
+            print("refresh")
+            epd.clearDisplay()
+            startTime = time.time()
+        image = display.displayAll(config['apiKeys']['weatherAPI'], config['apiKeys']['cityName'],'test1.png')
+        epd.displayImage(image) # send image to display
+        
+        time.sleep(config['updateIntervals']['globalTick'])
+        # time.sleep(10)
 
 if __name__ == "__main__":
     main()
