a6889b5e134b15163cab362af2df4b807193ed1d
Amish Patel <64919993+Toast7529@users.noreply.github.com>
2024-09-20 12:34:54 +0100

    Swapped red to black

--- display.py
+++ display.py
@@ -15,9 +15,9 @@ class Display:
         currentMemory = memory.used / (1024**3)  # fetch sys info
         maxMemory = memory.total / (1024**3)
         # get memory usage and percentage
-        self._draw.text((55,195), f"{int(currentMemory/maxMemory*100)}%  {round(currentMemory,1)}GiB", fill=(255,0,0))
+        self._draw.text((55,195), f"{int(currentMemory/maxMemory*100)}%  {round(currentMemory,1)}GiB", fill=(0,0,0))
         barWidth = int(currentMemory/maxMemory*110) + 3 # 113 is bar width
-        self._draw.line((8,212,barWidth,212), fill=(255,0,0), width=3)
+        self._draw.line((8,212,barWidth,212), fill=(0,0,0), width=3)
 
     def displayCPU(self):
         cpuInfo = psutil.cpu_freq()
@@ -31,10 +31,10 @@ class Display:
         currentSpeed = cpuInfo.current    # fetch sys info
         maxSpeed = cpuInfo.max
         # display CPU percentage and temp
-        self._draw.text((55,163), f"{int(currentSpeed/maxSpeed*100)}%  {temp}°C ", fill=(255,0,0))
+        self._draw.text((55,163), f"{int(currentSpeed/maxSpeed*100)}%  {temp}°C ", fill=(0,0,0))
 
         barWidth = int(currentSpeed/maxSpeed*110) + 3 # 113 is bar width
-        self._draw.line((8,180,barWidth,180), fill=(255,0,0), width=3)
+        self._draw.line((8,180,barWidth,180), fill=(0,0,0), width=3)
 
     def displayWeather(self, cityName, apiKey):
         res = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=metric")
@@ -45,7 +45,7 @@ class Display:
         temp = data['main']['temp']
 
         font = ImageFont.truetype("./Fonts/arial.ttf", 20)
-        self._draw.text((10,120), f"{round(float(temp),1)}°C", fill=(255,0,0), font=font)
+        self._draw.text((10,120), f"{round(float(temp),1)}°C", fill=(0,0,0), font=font)
         iconID = data["weather"][0]["icon"].replace("n","d")
         iconImage = Image.open(f"Assets/WeatherIcons/{iconID}.png").convert('RGBA')
         self._image.paste(iconImage, (80,115), iconImage)
@@ -56,9 +56,9 @@ class Display:
         time = now.strftime("%I:%M%p")
         date = now.strftime("%A, %b %d")
         font = ImageFont.truetype("./Fonts/consola.ttf", 25)
-        self._draw.text((10,20), time, fill=(255,0,0), font=font)
+        self._draw.text((10,20), time, fill=(0,0,0), font=font)
 
-        self._draw.text((10,45), date, fill=(255,0,0))
+        self._draw.text((10,45), date, fill=(0,0,0))
 
     def displayFace(self):
         currentFace = random.choice(self.faces)
